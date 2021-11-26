1bc41208e23e701162a17fbf7ba53617b8801c9d
friedpolypeptide <94933666+friedpolypeptide@users.noreply.github.com>
2021-11-26 02:17:58 -0600

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // ask user for a positive integer height of the pyramid (y)
+    int y = 0;
+    while (y < 1 || y > 8)
+    {
+        y = get_int ("height of pyramid?\n");
+    }
+    
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < y - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
